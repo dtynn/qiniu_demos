19890fa570a8b67abf5107a14225a959d7d361f1
dtynn <dtynn@163.com>
2013-09-17 17:26:50 +0800

    apis

--- py/apis/apis.py
+++ py/apis/apis.py
@@ -5,6 +5,7 @@ sys.setdefaultencoding('utf-8')
 sys.path.insert(0, '../')
 
 from base64 import urlsafe_b64decode, b64decode
+import json
 
 from qiniu import conf, rs
 
@@ -16,10 +17,35 @@ from tornado.httpserver import HTTPServer
 #http server
 PORT = 50021
 
+#qiniu
+ACCESS_KEY = ''
+SECRET_KEY = ''
+BUCKET = 'for-temp'
+DOMAIN = 'for-temp.qiniudn.com'
+
 
 class makeTokenHdl(tornado.web.RequestHandler):
     def get(self):
+        jsonp = self.get_argument('jsonp', 'callback')
         self.set_header('Content-Type', 'application/json')
+        conf.ACCESS_KEY = ACCESS_KEY
+        conf.SECRET_KEY = SECRET_KEY
+        policy = rs.PutPolicy(BUCKET)
+        policy.callbackUrl = 'http://dev-api.tuktalk.com/apis/cb'
+        policy.callbackBody = 'key=$(etag)'
+        #policy.persistentOps = 'avthumb/mp3/ar/44100/ab/32k;avthumb/mp3/aq/6/ar/16000'
+        #policy.persistentNotifyUrl = ''
+        uploadToken = policy.token()
+        self.write('%s(%s)' % (jsonp, json.dumps(uploadToken)))
+        return
+
+
+class callbackForImage(tornado.web.RequestHandler):
+    def post(self):
+        key = self.get_argument('key', '')
+        self.set_header('Content-Type', 'application/json')
+        url = 'http://%s/%s' % (DOMAIN, key)
+        self.write(json.dumps(url))
         return
 
 settings = dict(
@@ -31,7 +57,7 @@ settings = dict(
 
 
 urls = [
-
+    (r'/apis/make_uptoken', makeTokenHdl),
 ]
 
 app = tornado.web.Application(urls, **settings)
