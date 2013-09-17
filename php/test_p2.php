e5a1932eb96478ad988d78f31dca4c1c65bd67c0
dtynn <dtynn@163.com>
2013-09-17 10:40:05 +0800

    可行

--- php/test_p2.php
+++ php/test_p2.php
@@ -3,21 +3,26 @@
 require_once('qiniu/rs.php');
 require_once('qiniu/http.php');
 require_once('qiniu/auth_digest.php');
+require_once('qiniu/utils.php');
 
 
-$accessKey = 'iguImegxd6hbwF8J6ij2dlLIgycyU4thjg-xmu9q';
-$secretKey = 'EXJInB-eR0nkOwFet9uwP89MNzSpNXVqBoSh1yBo';
+$accessKey = '';
+$secretKey = '';
 Qiniu_SetKeys($accessKey, $secretKey);
+$mac = new Qiniu_Mac($accessKey, $secretKey);
 
-$url = 'http://t-test2.qiniudn.com/t1.mp3?p/2/avthumb/mp3/ar/44100/ab/32k';
+$url = "http://t-test2.qiniudn.com/t1.mp3?p/2/avthumb/mp/ar/44100/ab/32k";
+
 $size = strlen($url);
-#$ret = Qiniu_Client_Call($client, $url);
-$accessToken = Qiniu_Sign(null, $url);
+#$accessToken = Qiniu_Sign(null, $url);
+$accessToken = $mac->Sign("/t1.mp3?p/2/avthumb/mp/ar/44100/ab/32k\n");
+
 echo $accessToken . "\n";
 
 $u = array('path' => $url);
 $req = new Qiniu_Request($u, null);
 $req->Header['Authorization'] = "QBox $accessToken";
+$req->Header['Content-Type'] = "application/x-www-form-urlencoded";
 $req->Header['Content-Length'] = "0";
 echo 'start' . "\n";
 $ret = Qiniu_Client_do($req);
